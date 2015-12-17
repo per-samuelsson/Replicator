3bc5dc4ce9070c03d26c4de4e6c1c93039ee34a8
Johan Lindh <johan@linkdata.se>
2015-12-17 13:56:01 +0100

    instatiate tests

--- Program.cs
+++ Program.cs
@@ -249,6 +249,7 @@ namespace Replicator
             Db.Transact(() => { GetConfiguration(); }); // ensure that configuration object is created
             Status = "Not connected.";
             new HttpHandlers();
+            new ReplicationTests();
             _server = new ReplicationParent(_servermanager, _cts.Token);
         }
     }
